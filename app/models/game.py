b5413d5b82010ebc928094432f80b35ef2f6d172
Pedram Valiani <p.valiani@gmail.com>
2020-11-20 18:12:38 +0000

    added game test for creation

--- app/models/game.py
+++ app/models/game.py
@@ -6,14 +6,27 @@ from app.models.player import *
 
 class Game():
 
-    # - Initialise variables for the Game class
-    
-    def __init__(self, player_1, player_2, player_1_choice, player_2_choice):
+    # - Initialise variables for the Game class 
+
+    # - The class will take in two object instances of Player
+
+
+    def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
-        self.player_1_choice = player_1_choice
-        self.player_2_choice = player_2_choice
+        self.result = ''
 
 
-    def determine_winner(self, player_1, player_2, player_1_choice, player_2_choice):
-        pass
+    def play_game(self, player_1, player_2):
+    
+    # - The winner is determined based on the rules and player choices - can refactor this later
+        if self.player_1.choice == self.player_2.choice:
+            self.result = "The game is a tie"
+
+        elif(self.player_1.choice == "rock" and self.player_2.choice == "scissors") or \
+            (self.player_1.choice == "scissors" and self.player_2.choice == "paper") or \
+            (self.player_1.choice == "paper" and self.player_2.choice == "rock"):
+            self._result =  f"{self.player_1.name} has won!"
+        
+        else:
+            self.result = f"{self.player_2.name} has won!"
--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -9,4 +9,14 @@ from app.models.player import *
 
 class GameTest(unittest.TestCase):
     def setUp(self):
+        self.player_1 = Player("Player 1", "Rock")
+        self.player_2 = Player("Player 2", "Scissors")
+        self.game = Game(self.player_1, self.player_2)
+        
+
+    def test_a_game_is_created(self):
+        game_choices = self.player_1.choice + " vs " + self.player_2.choice
+        self.assertEqual("Rock vs Scissors", game_choices)
+
+    def test_game_is_a_draw(self):
         pass

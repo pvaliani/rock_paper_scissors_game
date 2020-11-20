ed5d9c1654ca4b507d265792b8eaaf1151f235ad
Pedram Valiani <p.valiani@gmail.com>
2020-11-20 17:08:17 +0000

    adds game and class definitions

---
+++ app/models/game.py
@@ -0,0 +1,6 @@
+# - Class definition for the Rock, Paper, Scissors game
+
+class Game():
+
+    def __init__(self, player_1, player_2, choice_1, choice_2):
+        pass
---
+++ app/models/player.py
@@ -0,0 +1,7 @@
+# - Class definition for a player of the Rock, Paper, Scissors game
+
+class Player():
+
+    def __init__(self, name, choice):
+        self.name = name
+        self.choice = choice

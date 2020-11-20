df5d249e6dceed1bf506bb17ebcb693f55a8e0e4
Pedram Valiani <p.valiani@gmail.com>
2020-11-20 17:37:00 +0000

    add determine winner

--- app/models/game.py
+++ app/models/game.py
@@ -8,5 +8,12 @@ class Game():
 
     # - Initialise variables for the Game class
     
-    def __init__(self, player_1, player_2, choice_1, choice_2):
+    def __init__(self, player_1, player_2, player_1_choice, player_2_choice):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.player_1_choice = player_1_choice
+        self.player_2_choice = player_2_choice
+
+
+    def determine_winner(self, player_1, player_2, player_1_choice, player_2_choice):
         pass

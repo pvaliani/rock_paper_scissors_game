eb4ca70569c6cc71820264cc91e3f0e1d6e66a3a
Pedram Valiani <p.valiani@gmail.com>
2020-11-20 18:50:17 +0000

    comments updated

--- app/models/game.py
+++ app/models/game.py
@@ -14,19 +14,26 @@ class Game():
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
-        self.result = ''
+        
 
 
     def play_game(self, player_1, player_2):
     
     # - The winner is determined based on the rules and player choices - can refactor this later
-        if self.player_1.choice == self.player_2.choice:
-            self.result = "The game is a tie"
 
-        elif(self.player_1.choice == "rock" and self.player_2.choice == "scissors") or \
-            (self.player_1.choice == "scissors" and self.player_2.choice == "paper") or \
-            (self.player_1.choice == "paper" and self.player_2.choice == "rock"):
-            self._result =  f"{self.player_1.name} has won!"
+        if (self.player_1.choice == self.player_2.choice):
+            result = "None"
+
+        elif(( self.player_1.choice == "Rock" and self.player_2.choice =="Paper" ) or ( self.player_1.choice == "Paper" and self.player_2.choice =="Rock" )):
+            result = "Paper"
         
+        elif(( self.player_1.choice == "Scissors" and self.player_2.choice =="Paper" ) or ( self.player_1.choice == "Paper" and self.player_2.choice =="Scissors" )):
+            result= "Scissors"
+
         else:
-            self.result = f"{self.player_2.name} has won!"
+            result= "Rock"
+
+        if result == self.player_1.choice:
+            return player_1.name
+        else:
+            return player_2.name
--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -13,7 +13,7 @@ class GameTest(unittest.TestCase):
 # - Define player_1 and player_2 objects based on the Player class which have their respective choices
 
         self.player_1 = Player("Player 1", "Rock")
-        self.player_2 = Player("Player 2", "Scissors")
+        self.player_2 = Player("Player 2", "Rock")
 
 # - Define a game as having player_1 and player_2 competing
 
@@ -23,10 +23,26 @@ class GameTest(unittest.TestCase):
 
     def test_a_game_is_created(self):
         game_choices = self.player_1.choice + " vs " + self.player_2.choice
-        self.assertEqual("Rock vs Scissors", game_choices)
+        self.assertEqual("Rock vs Rock", game_choices)
 
-# - Tests whether the game is a draw or not by comparing the two player choices 
+# - Tests whether the game is a draw or not by comparing the two player choices - could refactor this with a function 
 
     def test_game_is_a_draw(self):
-        self.player_2 = Player("Player 2", "Rock")
         self.assertEqual(self.player_1.choice, self.player_2.choice)
+
+# - Tests whether paper has beaten rock in the game - define two players with paper and rock choices, respectively and create a new game instance with the new values of player
+
+    def test_paper_beats_rock(self):
+        self.player_1 = Player("Player 1", "Paper")
+        self.player_2 = Player("Player 2", "Rock")
+        self.game = Game(self.player_1, self.player_2)
+
+# - Test that the values for paper and rock match each respective player choice
+
+        self.assertEqual("Paper", self.player_1.choice)
+        self.assertEqual("Rock", self.player_2.choice)
+        
+# - Test that Player 1 is the winner based on paper beating rock in the game
+
+        self.assertEqual("Player 1", self.game.play_game(self.player_1, self.player_2))
+        

05dce3d1d737f17695727a61d4e67ac65f42970d
Pedram Valiani <p.valiani@gmail.com>
2020-11-21 02:40:15 +0000

    soo...tired..right..now

--- app/controller.py
+++ app/controller.py
@@ -16,13 +16,14 @@ def base():
     
 # - The get request which matches the players choices to return the result of the game. If the user inputs their choices in the browser as /rock/scissors for example, then the result will be returned in the browser as a string
 
+@app.route('/result')
 @app.route('/<choice_1>/<choice_2>')
 def play_the_game(choice_1, choice_2):
     player_1 = Player("Player 1", choice_1)
     player_2 = Player("Player 2", choice_2)
     game = Game(player_1, player_2)
     winner = game.play_game()
-    return choice_1.title() + " " + choice_2.title() + " " + winner
+    return "Player 1 chose: " + choice_1.title() + " " + " Player 2 chose: " + choice_2.title() + "    " + " So, the winner is..." + winner
 
 #---------------------------------- MVP --------------------------------------------------------
 
